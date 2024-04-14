2228d4199af4eda7cef9a005631ef7787fac5285
yooneee <hullpo13@gmail.com>
2024-04-14 20:11:47 +0900

    gimpoupdate1

--- gimpop.py
+++ gimpop.py
@@ -3,52 +3,43 @@ import asyncio
 from aiohttp import web
 import aiohttp_cors
 import os
-import xmltodict  # xmltodict 라이브러리 추가
+import xmltodict  # XML을 JSON 형식으로 변환하기 위해 필요
 
 async def fetch_parking_data(session, url, params):
     async with session.get(url, params=params) as response:
         if response.status == 200:
-            # XML 응답을 JSON으로 변환
             text = await response.text()
-            data_dict = xmltodict.parse(text)
-            return json.loads(json.dumps(data_dict))  # Convert XML dict to JSON
+            data = xmltodict.parse(text)
+            return data
         else:
             return {"error": "Data fetching failed", "status": response.status}
 
-async def get_all_airport_parking(request):
-    service_key = os.getenv('SERVICE_KEY')
+async def get_airport_parking(request):
+    service_key = os.getenv('SERVICE_KEY_GIMPOPARK')
     base_url = "http://openapi.airport.co.kr/service/rest/AirportParkingCongestion/airportParkingCongestionRT"
-    num_of_rows = request.query.get('numOfRows', '10')
-    page_no = request.query.get('pageNo', '1')
+    params = {
+        'serviceKey': service_key,
+        'schAirportCode': 'GMP',
+        'numOfRows': '10',
+        'pageNo': '1',
+        '_type': 'json'
+    }
 
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        for airport_code in AIRPORT_CODES:
-            params = {
-                'serviceKey': service_key,
-                'schAirportCode': airport_code,
-                'numOfRows': num_of_rows,
-                'pageNo': page_no,
-                '_type': 'xml'  # 데이터 형식을 XML로 요청 (API가 JSON을 지원하지 않는 경우)
-            }
-            task = fetch_parking_data(session, base_url, params)
-            tasks.append(task)
-        
-        results = await asyncio.gather(*tasks)
-        return web.json_response(results)
+        result = await fetch_parking_data(session, base_url, params)
+        return web.json_response(result)
 
 app = web.Application()
 cors = aiohttp_cors.setup(app, defaults={
-    "https://dalyoon.com": aiohttp_cors.ResourceOptions(
+    "*": aiohttp_cors.ResourceOptions(
         allow_credentials=True,
         expose_headers="*",
         allow_headers="*",
     )
 })
 
-# 모든 공항 주차 혼잡도 정보 라우트 설정
-all_parking_resource = cors.add(app.router.add_resource("/all-airport-parking"))
-cors.add(all_parking_resource.add_route("GET", get_all_airport_parking))
+resource = cors.add(app.router.add_resource("/gimpo-parking"))
+cors.add(resource.add_route("GET", get_airport_parking))
 
 if __name__ == '__main__':
     web.run_app(app, port=int(os.getenv('PORT', 8000)))
